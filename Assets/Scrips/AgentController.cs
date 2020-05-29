5ed54181a6f978815583f2ca1943a50bc2844c97
Capoq1 <mrico9@cuc.edu.co>
2020-05-29 18:54:56 -0500

    core assets
    Core assets created

---
+++ Assets/Scrips/AgentController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AgentController : MonoBehaviour
+{
+    // Start is called before the first frame update
+  UnityEngine.AI.NavMeshAgent myNavMeshAgent;
+  public Transform target;
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      myNavMeshAgent.SetDestination(target.position);  
+    }
+
+}
